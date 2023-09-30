fd5987b06da5724908d78dc437e3792936088b90
ClaMor21 <118982205+ClaMor21@users.noreply.github.com>
2023-09-30 18:30:07 +0530

    Update Applyzer.py

--- Applyzer.py
+++ Applyzer.py
@@ -1,17 +1,18 @@
 import sys
 import requests
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QTextEdit, QVBoxLayout, QWidget, QFileDialog, QDialog, QLabel, QLineEdit, QMessageBox
+import json
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QTextEdit, QVBoxLayout, QWidget, QFileDialog, QMessageBox
 
 from androguard.core.bytecodes.apk import APK
 
-class ApplyzerGUI(QMainWindow):  # Changed the class name to ApplyzerGUI
+class ApplyzerGUI(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('Applyzer')  # Changed the window title to "Applyzer"
+        self.setWindowTitle('Applyzer')
         self.setGeometry(100, 100, 800, 600)
 
         self.text_output = QTextEdit(self)
@@ -26,20 +27,14 @@ class ApplyzerGUI(QMainWindow):  # Changed the class name to ApplyzerGUI
         self.clear_button = QPushButton('Clear', self)
         self.clear_button.clicked.connect(self.clear_output)
 
-        self.api_key_label = QLabel('Enter VirusTotal API Key:', self)
-        self.api_key_line_edit = QLineEdit(self)
-        self.api_key_line_edit.setPlaceholderText('API Key')
-
         self.pre_static_analysis_button = QPushButton('Pre-Static Analysis', self)
-        self.pre_static_analysis_button.clicked.connect(self.open_prestatic_analysis_dialog)
+        self.pre_static_analysis_button.clicked.connect(self.pre_static_analysis)
 
         layout = QVBoxLayout()
         layout.addWidget(self.text_output)
         layout.addWidget(self.analyze_button)
         layout.addWidget(self.browse_button)
         layout.addWidget(self.clear_button)
-        layout.addWidget(self.api_key_label)
-        layout.addWidget(self.api_key_line_edit)
         layout.addWidget(self.pre_static_analysis_button)
 
         container = QWidget()
@@ -60,19 +55,19 @@ class ApplyzerGUI(QMainWindow):  # Changed the class name to ApplyzerGUI
             harmful_permissions = [
                 'android.permission.CAMERA',
                 'android.permission.RECORD_AUDIO',
-                'android.permission.INTERNET1' ,
-                'android.permission.ACCESS_COARSE_LOCATION' , 
-                'android.permission.WRITE_CONTACTS'
-                'android.permission.SEND_SMS' ,
-                'android.permission.WRITE_CALL_LOG' , 
-                'android.permission.READ_CALL_LOG' , 
-                'android.permission.WRITE_EXTERNAL_STORAGE' ,
-                'android.permission.RECORD_AUDIO' ,
-                'android.permission.ACCESS_FINE_LOCATION' ,
-                'android.permission.CALL_PHONE' ,
+                'android.permission.INTERNET1',
+                'android.permission.ACCESS_COARSE_LOCATION',
+                'android.permission.WRITE_CONTACTS',
+                'android.permission.SEND_SMS',
+                'android.permission.WRITE_CALL_LOG',
+                'android.permission.READ_CALL_LOG',
+                'android.permission.WRITE_EXTERNAL_STORAGE',
+                'android.permission.RECORD_AUDIO',
+                'android.permission.ACCESS_FINE_LOCATION',
+                'android.permission.CALL_PHONE',
             ]
 
-            self.text_output.clear()  # Clear previous results
+            self.text_output.clear()
             self.text_output.append("Analysis Report for: " + self.apk_file)
             self.text_output.append("\nPotentially Harmful Permissions:")
 
@@ -83,67 +78,67 @@ class ApplyzerGUI(QMainWindow):  # Changed the class name to ApplyzerGUI
             if 'AndroidManifest.xml' in a.get_files():
                 self.text_output.append("AndroidManifest.xml found")
         except AttributeError:
-            self.text_output.clear()  # Clear previous results
+            self.text_output.clear()
             self.text_output.append("Please select an APK file for analysis.")
 
     def clear_output(self):
-        self.text_output.clear()  # Clear the analysis report
+        self.text_output.clear()
 
-    def open_prestatic_analysis_dialog(self):
-        api_key = self.api_key_line_edit.text()
-
-        if not api_key:
-            QMessageBox.critical(self, 'Error', 'Please provide a VirusTotal API key.')
-            return
-
-        dialog = PreStaticAnalysisDialog(self.apk_file, api_key)
-        dialog.exec_()
-
-class PreStaticAnalysisDialog(QDialog):
-    def __init__(self, apk_file, api_key):
-        super().__init__()
-
-        self.setWindowTitle('Pre-Static Analysis')
-        self.setGeometry(200, 200, 600, 400)
-
-        self.api_key = api_key
-        self.apk_file = apk_file
-
-        self.text_output = QTextEdit(self)
-        self.text_output.setReadOnly(True)
-
-        self.analyze_button = QPushButton('Analyze with VirusTotal', self)
-        self.analyze_button.clicked.connect(self.analyze_with_virustotal)
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.text_output)
-        layout.addWidget(self.analyze_button)
-        self.setLayout(layout)
-
-    def analyze_with_virustotal(self):
+    def pre_static_analysis(self):
         try:
             with open(self.apk_file, 'rb') as file:
-                response = self.scan_apk_with_virustotal(file, self.api_key)
+                response = self.scan_apk_with_virustotal(file)
 
-            self.text_output.clear()  # Clear previous results
+            self.text_output.clear()
             self.text_output.append("Pre-Static Analysis Report for: " + self.apk_file)
             self.text_output.append("\nVirusTotal Analysis:")
             self.text_output.append("Response: " + response)
 
+            # Fetch and display security vendors' analysis with only flagged vendors
+            flagged_vendors = self.fetch_flagged_virustotal_vendors(response)
+            self.text_output.append("\nFlagged Security Vendors' Analysis:")
+            self.text_output.append(flagged_vendors)
+
         except FileNotFoundError:
-            self.text_output.clear()  # Clear previous results
+            self.text_output.clear()
             self.text_output.append("Please select an APK file for analysis.")
 
-    def scan_apk_with_virustotal(self, file, api_key):
+    def scan_apk_with_virustotal(self, file):
+        # You can implement your VirusTotal API call here and return the response.
+        # Replace this with your actual VirusTotal API integration.
+
+        # For example, you might use requests to send the file to VirusTotal API:
         url = 'https://www.virustotal.com/vtapi/v2/file/scan'
-        params = {'apikey': api_key}
+        params = {'apikey': 'cabc5965983f934e1aff7a8a97bb5cf541575292adbaed7a6d5268d9d86bdad5'}
         files = {'file': file}
         response = requests.post(url, files=files, params=params)
         return response.text
 
+    def fetch_flagged_virustotal_vendors(self, report_response):
+        # Use your VirusTotal API key to fetch more information about the report.
+        # Filter and format the security vendors' analysis to show only flagged vendors.
+
+        # Get the resource ID from the initial scan response
+        response_data = json.loads(report_response)
+        resource_id = response_data['resource']
+
+        # Fetch security vendors' analysis using the resource ID
+        url = 'https://www.virustotal.com/vtapi/v2/file/report'
+        params = {'apikey': 'cabc5965983f934e1aff7a8a97bb5cf541575292adbaed7a6d5268d9d86bdad5', 'resource': resource_id}
+        response = requests.get(url, params=params)
+        response_data = json.loads(response.text)
+
+        # Extract and format security vendors' analysis for flagged vendors
+        analysis_text = "Flagged Security Vendors' Analysis:\n"
+        for vendor, result in response_data['scans'].items():
+            if result['result']:  # Check if the result field is not empty (vendor flagged it)
+                analysis_text += f"{vendor}: {result['result']}\n"
+
+        return analysis_text
+
 def main():
     app = QApplication(sys.argv)
-    window = ApplyzerGUI()  # Changed the class name here as well
+    window = ApplyzerGUI()
     window.show()
     sys.exit(app.exec_())
 
